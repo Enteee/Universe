70a470b7f9535f5391bfb6870755da5799b60c66
Ente <ducksource@duckpond.ch>
2015-05-19 00:46:56 +0200

    clean & format

--- src/main/java/ch/duckpond/universe/elements/Mass.java
+++ src/main/java/ch/duckpond/universe/elements/Mass.java
@@ -9,48 +9,48 @@ import org.newdawn.slick.geom.Vector2f;
 
 @Entity("masses")
 public class Mass {
-    
+
     @Id
     private ObjectId id;
     private int      mass;
     private Vector2f position;
     private Vector2f velocity;
-    
+
     public Mass(final int mass, final Vector2f position, final Vector2f velocity) throws InvalidActivityException {
         setMass(mass);
         setPosition(position);
         setVelocity(velocity);
     }
-    
+
     public ObjectId getId() {
         return id;
     }
-    
+
     public int getMass() {
         return mass;
     }
-    
-    public void setMass(int mass) throws InvalidActivityException {
+
+    public void setMass(final int mass) throws InvalidActivityException {
         if (mass <= 0) {
             throw new InvalidActivityException("mass <= 0");
         }
         this.mass = mass;
     }
-    
+
     public Vector2f getPosition() {
         return position;
     }
-    
-    public void setPosition(Vector2f position) {
+
+    public void setPosition(final Vector2f position) {
         this.position = position;
     }
-    
+
     public Vector2f getVelocity() {
         return velocity;
     }
-    
-    public void setVelocity(Vector2f velocity) {
+
+    public void setVelocity(final Vector2f velocity) {
         this.velocity = velocity;
     }
-    
+
 }
--- src/main/java/ch/duckpond/universe/simulation/Simulation.java
+++ src/main/java/ch/duckpond/universe/simulation/Simulation.java
@@ -12,16 +12,16 @@ import org.newdawn.slick.geom.Vector2f;
 import ch.duckpond.universe.elements.Mass;
 
 public class Simulation {
-    
+
     private static final Logger     logger = LogManager.getLogger(Simulation.class);
     private static final List<Mass> masses = new ArrayList<>();
-    
+
     public static void main(final String argv[]) {
-        
+
         logger.info("HELLO WORLD");
         try {
             masses.add(new Mass(10, new Vector2f(10, 0), new Vector2f(10, 0)));
-        } catch (InvalidActivityException e) {
+        } catch (final InvalidActivityException e) {
             logger.error("Mass test failed!");
         }
     }
